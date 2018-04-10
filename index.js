36df2a71ad8cb16233005fb43bde5ff8bda2d138
Dennnis Jade Toribio <dennis.toribio@viseo.com>
2018-04-10 17:44:10 +0800

    Remove filesystem lib

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
 var tokenVerify  = require('./lib/jwt');
-var fileSystem = require('./lib/filesystem');
-var loggerSystem = require('./lib/logger');
+var logger = require('./lib/logger');
+var parser = require('./lib/parser');
+
 
 module.exports = {
   token: tokenVerify,
-  file: fileSystem,
-  log: loggerSystem
+  logger: logger,
+  parser: parser
 };
