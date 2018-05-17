04b3df01f8ca4390491497c98fb3fca30a16969c
Dennnis Jade Toribio <dennis.toribio@viseo.com>
2018-05-17 15:41:02 +0800

    add mailer

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ var tokenVerify  = require('./lib/jwt');
 var logger = require('./lib/logger');
 var parser = require('./lib/parser');
 var config = require('./lib/config');
+var mailer = require('./lib/mailer');
 
 module.exports = {
   token: tokenVerify,
   logger: logger,
   parser: parser,
   config: config,
+  mailer: mailer,
 };
---
+++ src/mailer.js
@@ -0,0 +1,50 @@
+import nodemailer from 'nodemailer';
+import { log } from './logger'
+
+const defaultTo = 'dennis.toribio@viseo.com'
+const defaultFrom = 'mailer <robutils@mailer.com>'
+
+const getTransporter = (mailCreds) => new Promise((resolve) => {
+  try {
+    const transporter = nodemailer.createTransport(mailCreds);
+    resolve(transporter);
+  } catch (e) {
+    log.error('getTransporter', e)
+  }
+});
+
+const sendEmail = (mailOptions, smtpTransport) =>
+  new Promise((resolve, reject) => smtpTransport.sendMail(mailOptions, (error, response) => {
+    if (error) {
+      log.error(`Could not send email err: ${error}`);
+      return reject(error);
+    }
+    log.info(`Message sent: ${JSON.stringify(response.accepted)}`);
+    return resolve(response);
+  }));
+
+const sendEmailNotification = async (requestBody, from = defaultFrom, to = defaultTo, mailCreds) => {
+  try {
+    const transporter = await getTransporter(mailCreds);
+    const mailOptions = {
+      from,
+      to,
+      subject: `${requestBody.subject} (store name: ${config.STORE_NAME})`,
+      html: requestBody.body,
+    };
+    if (requestBody.filePath) {
+      mailOptions.attachments = [
+        {
+          path: requestBody.filePath,
+        },
+      ];
+    }
+    await sendEmail(mailOptions, transporter);
+    return true;
+  } catch (e) {
+    log.error('sendEmailNotification', e)
+    return e;
+  }
+};
+
+export default sendEmailNotification;
